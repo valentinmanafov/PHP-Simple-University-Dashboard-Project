5fd570bdd645eaa2c7cd1fdade47f675c1bd52d7
Valentin Manafov <itzbunnyfn@gmail.com>
2025-04-19 13:24:28 +0300

    Basic project formating and desing

--- index.php
+++ index.php
@@ -7,37 +7,41 @@
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="style.css">
 
+    <script src="https://kit.fontawesome.com/dd71a00e9a.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>WEBversity</title>
 </head>
 
 <body>
     <div class="site-body">
         <div class="menu-left">
-            <div class="nav">
-                <h4 class="menu-title">
-                    WEBversity Dashboard
-                </h4>
+            <img src="img\logo.png" alt="" class="menu-img">
+            <div class="menu-nav">
                 <ul class="menu-ul">
                     <li class="menu-li">
-                        <form action=""><button class="menu-buttons">Students</button></form>
+                        <form action=""><button class="menu-buttons"><i class="fa-solid fa-user"></i><span>Students</span></button></form>
                     </li>
                     <li class="menu-li">
-                        <form action=""><button class="menu-buttons">Professors</button></form>
+                        <form action=""><button class="menu-buttons"><i class="fa-solid fa-user-graduate"></i><span>Professors</span></button></form>
                     </li>
                     <li class="menu-li">
-                        <form action=""><button class="menu-buttons">Subjects</button></form>
+                        <form action=""><button class="menu-buttons"><i class="fa-solid fa-book"></i><span>Subjects</span></button></form>
                     </li>
                     <li class="menu-li">
-                        <form action=""><button class="menu-buttons">Grades</button></form>
+                        <form action=""><button class="menu-buttons"><i class="fa-solid fa-clipboard"></i><span>Grades</span></button></form>
                     </li>
                     <li class="menu-li">
-                        <form action=""><button class="menu-buttons">Advanced Reports</button></form>
+                        <form action=""><button class="menu-buttons"><i class="fa-solid fa-chart-simple"></i><span>Advanced Reports</span></button></form>
                     </li>
                 </ul>
             </div>
         </div>
         <div class="site-content">
-            <h1>testttt</h1>
+            <div class="section1">
+                <h1>This is a test of the ui !!!</h1>
+            </div>
         </div>
     </div>
 
