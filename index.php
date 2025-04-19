1041224f1086fd8608343053456adb8de05b5c30
Valentin Manafov <itzbunnyfn@gmail.com>
2025-04-19 11:22:57 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="rest.css">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>WEBversity</title>
+</head>
+<body>
+    <div class="site-body">
+    <div class="menu-left">
+        <div class="nav">
+            <h4 class="menu-title">
+            WEBversity Dashboard
+            </h4>
+                <ul class="menu-ul">
+                    <li class="menu-li"><form action=""><button class="menu-buttons">Students</button></form></li>
+                    <li class="menu-li"><form action=""><button class="menu-buttons">Professors</button></form></li>
+                    <li class="menu-li"><form action=""><button class="menu-buttons">Subjects</button></form></li>
+                    <li class="menu-li"><form action=""><button class="menu-buttons">Grades</button></form></li>
+                    <li class="menu-li"><form action=""><button class="menu-buttons">Advanced Reports</button></form></li>
+                </ul>
+                </div>
+       </div>
+    <div class="site-content">
+        <h1>testttt</h1>
+    </div>
+    </div>
+
+
+
+
+    <?php
+    ?>
+
+</body>
+</html>
